283b362955feac3661ef3b705de3f00961436a15
Windy <windywu812@gmail.com>
2020-10-27 15:00:27 +0700

    Initial Commit

---
+++ Submission-Capstone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Submission-Capstone
+//
+//  Created by Windy on 27/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
